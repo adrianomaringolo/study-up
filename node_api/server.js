cbfb617769b36c71ade95bc6b113a19f498437ac
Rodrigo Rampazzo <rtedeschi.dextra@vstsalelo.onmicrosoft.com>
2020-12-18 11:37:12 -0300

    Geração automatica de referências de end-points no Swagger

--- node_api/server.js
+++ node_api/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./src/config/Swagger/swagger.yaml');
+// const YAML = require('yamljs');
+// const swaggerFile = YAML.load('./src/config/Swagger/swagger.yaml');
+const swaggerFile = require('./src/config/Swagger/swagger_output.json');
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 require('./src/config/Routes/index')(app);
 
 app.use(cors());
 app.use(express.json());
-app.listen(3333);
+app.listen(3000);
--- node_api/src/features/Usuario/UsuarioController.js
+++ node_api/src/features/Usuario/UsuarioController.js
@@ -1,22 +1,56 @@
 exports.post = (req, res, next) => {
-   res.status(201).send('Rota POST!');
+   // #swagger.tags = ['User']
+   // #swagger.description = 'Endpoint para adicionar um usuário.'
+
+   /* #swagger.parameters['newUser'] = {
+         in: 'body',
+         description: 'Informações do usuário.',
+         required: true,
+         type: 'object',
+         schema: { $ref: "#/definitions/AddUser" }
+      } */
+
+        const newUser = req.body
+
+        if (true) {
+            // #swagger.responses[201] = { description: 'Usuário registrado com sucesso!' }
+            return res.status(201).send('Rota POST!')
+        }
+        return res.status(500)    // #swagger.responses[500]
 };
  
 exports.put = (req, res, next) => {
+   // #swagger.tags = ['User']
+   // #swagger.description = 'Endpoint para atualizar um usuário.'
    let id = req.params.id;
    res.status(201).send(`Rota PUT com ID! --> ${id}`);
 };
  
 exports.delete = (req, res, next) => {
+   // #swagger.tags = ['User']
+   // #swagger.description = 'Endpoint para apagar um usuário.'
    let id = req.params.id;
    res.status(200).send(`Rota DELETE com ID! --> ${id}`);
 };
  
 exports.get = (req, res, next) => {
+   // #swagger.tags = ['User']
+   // #swagger.description = 'Endpoint para buscar usuários.'
    res.status(200).send('Rota GET!');
 };
  
 exports.getById = (req, res, next) => {
+   // #swagger.tags = ['User']
+   // #swagger.description = 'Endpoint para buscar um usuário.'
+   /* #swagger.parameters['id'] = {
+         description: 'ID do usuário.',
+         type: 'number'
+      } */
    let id = req.params.id;
+
+   /* #swagger.responses[200] = { 
+         schema: { $ref: "#/definitions/User" },
+         description: 'Usuário encontrado.' 
+      } */
    res.status(200).send(`Rota GET com ID! ${id}`);
 };
